fe9e4cee77ebe7e65e2cbef2c73fe3477678d568
Lianet Sepulveda Torres <lisepul@umich.edu>
2023-11-01 17:17:51 -0400

    create a shell script to create collection and indexing data

--- solrcloud_setup/collection_manager.sh
+++ solrcloud_setup/collection_manager.sh
@@ -1,36 +1,13 @@
 #!/bin/bash
-export $(grep -v '^#' .env | xargs -d '\n')
+export $(grep -v '^#' env.example | xargs -d '\n')
 
-echo "Generating collections!!"
+#echo "SOLR variables"
+#export user=solr
+#export password=SolrRocks
+#export host="http://localhost:8983"
 
-#cd conf
+echo "Creating collection"
+curl -u $user:$password "$host/solr/admin/collections?action=CREATE&name=core-x&instanceDir=/var/solr/data/core-x&numShards=1&collection.configName=core-x&dataDir=/var/solr/data/core-x"
 
-
-#rm core-x.zip
-
-
-#zip -r ../core-x.zip .
-
-#cd ..
-
-echo "SOLR variables"
-export user=solr
-export password=SolrRocks
-export host="http://localhost:8983"
-
-#curl -u solr:SolrRocks -X DELETE "http://localhost:8983/api/cluster/configs/core-x"
-#curl -u solr:SolrRocks -X PUT --header "Content-Type:application/octet-stream" --data-binary @core-x.zip "http://localhost:8983/api/cluster/configs/core-x"
-
-#curl -u $user:$password "$host/solr/admin/collections?action=DELETE&name=core-x"
-#curl -u $user:$password "$host/solr/admin/collections?action=CREATE&name=core-x&numShards=1&collection.configName=core-x"
-
-#Creating collection
-curl -u solr:SolrRocks "http://localhost:8983/solr/admin/collections?action=CREATE&name=core-x&instanceDir=/var/solr/data/core-x&numShards=1&collection.configName=core-x&dataDir=/var/solr/data/core-x"
-
-#Indexing documents
-
-
-############
-
-#curl -u solr:SolrRocks -X DELETE "http://localhost:8983/api/cluster/configs/core-x"
-#curl -u solr:SolrRocks -X PUT --header "Content-Type:application/octet-stream" --data-binary @core-x.zip "http://localhost:8983/api/cluster/configs/core-x"
+echo "Indexing documents"
+curl -u $user:$password "$host/solr/core-x/update?commit=true" --data-binary @solr_json_documents/core-data.json -H 'Content-type:application/json'

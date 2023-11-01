c8514a7433be5e11b0ed1b523f1c92ebad982f9f
Lianet Sepulveda Torres <lisepul@umich.edu>
2023-10-31 22:53:09 -0400

    create a collection through ZooKeeper CLI

--- solrcloud_setup/init_files/solr_init.sh
+++ solrcloud_setup/init_files/solr_init.sh
@@ -3,8 +3,11 @@
 # enables solr to start with basic auth turned on
 solr zk cp /var/solr/data/security.json zk:security.json -z zoo1:2181
 
-# uploads the configuration in the authority_browse directory
+# uploads the configuration in the core-x directory
 solr zk upconfig -z  zoo1:2181 -n core-x -d /core-x
+
+# Link a collection to a Configset
+solr zk link-config -z zoo1:2181 -c core-x -n core-x
 
 echo "Starting up Solr and Zookeeper!!!"
 
